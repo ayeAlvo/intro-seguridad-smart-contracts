7095931e65edbd53fbc91d4c5c08c9815118a548
ayeAlvo <ayealvo@gmail.com>
2023-03-28 11:51:07 -0300

    test(BasicToken): finished functions

--- test/ejercicio-0/BasicToken.spec.js
+++ test/ejercicio-0/BasicToken.spec.js
@@ -1,35 +1,40 @@
-const { ethers } = require("hardhat");
-const { expect } = require("chai");
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
 
-describe("Ejercicio 0", function () {
+describe('Ejercicio 0', function () {
     // Cantidad inicial de 1 millón de tokens (1000000 * 10^18)
     const INITIAL_SUPPLY = ethers.utils.parseUnits('1000000', 'ether');
-    
+
     let deployer, usuario, otroUsuario;
 
     beforeEach(async function () {
         [deployer, usuario, otroUsuario] = await ethers.getSigners();
 
-        const BasicToken = await ethers.getContractFactory("BasicToken", deployer);
+        const BasicToken = await ethers.getContractFactory(
+            'BasicToken',
+            deployer
+        );
 
         this.token = await BasicToken.deploy(INITIAL_SUPPLY);
     });
 
-    describe("Inicialización", function () {
-        it.skip('La cuenta minter es el deployer', async function () {
-            // COMPLETAR
+    describe('Inicialización', function () {
+        it('La cuenta minter es el deployer', async function () {
+            expect(await this.token.minter()).to.eq(deployer.address);
         });
 
         it('El total supply del token es el esperado', async function () {
             expect(await this.token.totalSupply()).to.eq(INITIAL_SUPPLY);
         });
 
-        it.skip('Todo el total supply es asignado al deployer', async function () {
-            // COMPLETAR
+        it('Todo el total supply es asignado al deployer', async function () {
+            expect(await this.token.balanceOf(deployer.address)).to.eq(
+                INITIAL_SUPPLY
+            );
         });
     });
 
-    describe("Transferencias", function() {
+    describe('Transferencias', function () {
         it('Un usuario sin fondos no puede transferir', async function () {
             expect(await this.token.balanceOf(usuario.address)).to.eq(0);
             await expect(
@@ -44,13 +49,15 @@ describe("Ejercicio 0", function () {
         });
     });
 
-    describe("Minting", function() {
-        it.skip('Un usuario sin permisos no puede mintear tokens', async function () {
-            // COMPLETAR
+    describe('Minting', function () {
+        it('Un usuario sin permisos no puede mintear tokens', async function () {
+            await expect(
+                this.token.connect(usuario).mint(otroUsuario.address, 100)
+            ).to.be.revertedWith('No permitido');
         });
 
-        it.skip('Un usuario con permisos puede mintear tokens', async function () {
-            // COMPLETAR
+        it('Un usuario con permisos puede mintear tokens', async function () {
+            await this.token.mint(usuario.address, 100);
         });
     });
 });
