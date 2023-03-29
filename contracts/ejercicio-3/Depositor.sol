d414f213fb5f8fc140ae8d2c19ab8cc5cb81d3c9
ayeAlvo <ayealvo@gmail.com>
2023-03-29 01:46:15 -0300

    feat: add function withdrawAllEther and comments

--- contracts/ejercicio-3/Depositor.sol
+++ contracts/ejercicio-3/Depositor.sol
@@ -17,17 +17,25 @@ contract Depositor is Ownable {
     }
 
     function depositToVault() external payable onlyOwner {
-        // COMPLETAR
+        vault.deposit{value: msg.value}();
         // La sintaxis para enviar ETH de un contrato a otro podés verla acá https://docs.soliditylang.org/en/v0.8.7/control-structures.html#external-function-calls
     }
 
     function withdrawFromVault() external onlyOwner {
-        // COMPLETAR
+        vault.withdraw();
+    }
+
+    function withdrawAllEther() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
     }
 
     // 1. ¿Por qué es necesaria esta función?
+    // sirve para que un contrato pueda recibir ether de otro contrato
     // 2. ¿Qué pasa si la borramos?
-    // 3. Aún así, ¿es suficiente? ¿No necesitamos lógica adicional?
+    //  el contrato no va a poder recibir ether a traves de .transfer
+    // 3. Aún así, ¿es suficiente? ¿No necesitamos lógica adicional? no, si
     // 4. De ser necesaria, ¿podemos incluir esa lógica acá?
+    //  no, en otra funcion, como withdrawAllEther
+    //  reverts on failure, forwards 2300 gas stipend, not adjustable
     receive() external payable {}
 }
