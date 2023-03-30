3b00b663120a4e5f3841309bbc3c07cea34f8fd6
ayeAlvo <ayealvo@gmail.com>
2023-03-29 23:29:13 -0300

    feat: add console.log and ReentrancyGuard

--- contracts/ejercicio-5/VaultV3.sol
+++ contracts/ejercicio-5/VaultV3.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "hardhat/console.sol";
 
-contract VaultV3 is Ownable {
+contract VaultV3 is Ownable, ReentrancyGuard {
 
   using Address for address payable;
 
@@ -20,13 +22,18 @@ contract VaultV3 is Ownable {
   /**
     @notice Retirar ETH depositado
    */
-  function withdraw() external {
+  function withdraw() external nonReentrant {
     uint256 amountDeposited = deposits[msg.sender];
+    console.log(address(this).balance);
+
+    // Checks Effects Interaction Pattern
+    // deposits[msg.sender] = 0;
 
     // La función `sendValue` está disponible porque estamos usando la libreria Address de OpenZeppelin Contracts
     // Más detalles sobre esta función en https://docs.openzeppelin.com/contracts/4.x/api/utils#Address-sendValue-address-payable-uint256-
     payable(msg.sender).sendValue(amountDeposited);
 
     deposits[msg.sender] = 0;
+    console.log("Se setea en 0 el msg.value");
   }
 }
