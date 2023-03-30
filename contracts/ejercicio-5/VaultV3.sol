caaeab6a12f23c55560f8d98ed23446a677d5b07
ayeAlvo <ayealvo@gmail.com>
2023-03-29 23:30:40 -0300

    chore: delete unused import

--- contracts/ejercicio-5/VaultV3.sol
+++ contracts/ejercicio-5/VaultV3.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
-contract VaultV3 is Ownable, ReentrancyGuard {
+contract VaultV3 is ReentrancyGuard {
 
   using Address for address payable;
 
